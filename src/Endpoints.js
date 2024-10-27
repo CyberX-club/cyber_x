1bfbe2ed89928ce8a1441a6a48999016a65326be
Akshit shubham <121078566+shubhamakshit@users.noreply.github.com>
2024-10-27 18:18:57 +0000

    Update Endpoints.js

--- src/Endpoints.js
+++ src/Endpoints.js
@@ -1,7 +1,7 @@
 import {getApiUrl} from "./Api";
 
 class Endpoints {
-    static base_url = 'https://cyber-x-api.onrender.com'
+    static base_url = 'https://cyber-x-api.vercel.app'
 
     static get_thumbnail = (file_id) => (`${this.base_url}/file/${file_id}/thumbnail`);
 
