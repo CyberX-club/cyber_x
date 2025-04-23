299bf727355d8028d7216d8db09c1b771ceb3d2d
CyberX-club <cybersecurity@dpsrkp.net>
2025-04-23 16:07:55 +0530

    Update Alumni.js

--- src/components/Alumni.js
+++ src/components/Alumni.js
@@ -1,1 +1,24 @@
-
+  2024-25: [
+    { name: "Atharva Singh", role: "Director", image: "ATHARVA SINGH.jpeg" },
+    { name: "Gyanshu Raj", role: "Director", image: "Gyanshu Raj 12 E.png" },
+    {
+      name: "Atharvajeet Singh",
+      role: "Chief Advisor",
+      image: "ATHARVAJEET.jpeg",
+    },
+    { name: "Twamadi Sar", role: "Creative Head", image: "Twamadi Sar.jpg" },
+    {
+      name: "Akshit Shubham",
+      role: "Technical Head",
+      image: "Akshit Shubham.jpeg",
+    },
+    {
+      name: "Swastika Dubey",
+      role: "Event Head",
+      image: "Swastika Dubey12 F.jpg",
+    },
+    { name: "Vrinda Aggarwal", role: "Treasurer", image: "Vrinda.jpg" },
+    { name: "Tanush Jayara", role: "Member", image: "Tanush Jayara 12 E.png" },
+    { name: "Aaahana Mehrotra", role: "Member", image: "Aaahana Mehrotra.jpg" },
+    { name: "Vishisht", role: "Member", image: "Vishisht  12L .JPG" },
+  ],
