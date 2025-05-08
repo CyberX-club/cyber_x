1771cddd92211ab4a70476036748a3c0215d2605
CyberX-club <cybersecurity@dpsrkp.net>
2025-05-08 11:42:31 +0530

    Update MyRoutes.js

--- src/MyRoutes.js
+++ src/MyRoutes.js
@@ -1,116 +1,84 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import App from "./App";
 import Quiz from "./Quiz";
 import Navbar from "./Navbar";
+import React from "react";
 import CyberXApp, { AlumniApp } from "./components/Members";
 import { Box, Container, Switch, Typography } from "@mui/material";
 import Footer from "./Footer";
 import About from "./components/About";
 import Resources from "./components/Resources";
 import MagzineEntry from "./components/MagzineEntry";
-import InterviewEntry from "./InterviewEntry";
+import  InterviewEntry from "./InterviewEntry";
 import Magazine from "./components/Magzine";
+import Contact from './components/Contact';
 
-const email = "cybersecurity@dpsrkp.net";
 
-const socialMedia = [
-  { platform: "Facebook", url: "https://www.facebook.com/CyberX" },
-  { platform: "Twitter", url: "https://twitter.com/CyberX" },
-  { platform: "LinkedIn", url: "https://www.linkedin.com/company/cyberx" },
-  { platform: "GitHub", url: "https://github.com/CyberX-club/" },
-  { platform: "Instagram", url: "https://www.instagram.com/CyberX.CLUB" },
-];
+const MyRoutes = () => {
+  const navConfig = {
+    title: "CyberX",
+    menuItems: [
+      { label: "Home", href: "/" },
+      { label: "About", href: "/about" },
+      { label: "Members", href: "/members" },
+      { label: "Alumni", href: "/alumini" },
+      { label: "Resources", href: "/resources" },
+      { label: "Contact", onClick: () => console.log("Contact clicked") },
+    ],
+  };
 
-const resources = [
-  {
-    label: "Shielding Cyber Attacks",
-    img: "protection_against.png",
-    description:
-      "Guarding the Future: Your First Line of Defense Against Cyber Attacks",
-    author: "Goutam Behera",
-    postedAt: "2024-07-24",
-    urls: [
-      "https://drive.google.com/file/d/1zVQ4w58pCVHm200fSv9PkDMwW8ET3deU/view?usp=drive_link",
-    ],
-  },
-  {
-    label: "Cyber Security Awareness Guide",
-    description:
-      "A comprehensive guide on the best practices for staying safe online, covering topics such as password management, phishing, and social engineering.",
-    img: "ser-bg2.jpg",
-    author: "Atharva Singh",
-    postedAt: "2024-10-15",
-    urls: [
-      "https://drive.google.com/file/d/1CBtkmEu_aHNBnOQJqCHB02_py61Ed1PD/view?usp=sharing",
-    ],
-  },
-  {
-    label: "Intro To Trending Cyber Crime Attacks Over Network",
-    description:
-      "Your Guide to the Top Web Application Security Risks and How to Defend Against Them: A Comprehensive Resource on the Latest Network Security Threats",
-    img: "ser-bg.jpg",
-    author: "Goutam Behera",
-    postedAt: "2024-07-24",
-    urls: [
-      "https://drive.google.com/file/d/19lYngEhdcWWgF4PEE_4W4-nzzBxnpg4Z/view?usp=sharing",
-    ],
-  },
-  {
-    label: "Introduction to Network Security",
-    description:
-      "This PDF resource covers the fundamentals of network security, including firewalls, intrusion detection systems, and secure protocols.",
-    author: "Atharva Singh",
-    img: "ser-bg.jpeg",
-    urls: [
-      "https://drive.google.com/file/d/1hGLfmLrqDPV1cxBc6HdeLU4cLPxGPMZ2/view?usp=sharing",
-    ],
-  },
-  {
-    label: "DPSRKP Cyber Security Brochure",
-    description:
-      "Stay up-to-date with the latest news and trends in the world of cyber security, including recent cyber attacks, vulnerabilities, and patches.",
-    img: "dpsrkpcyberx.png",
-    urls: [
-      "https://drive.google.com/file/d/1fUkmDSicRRzUAl6R1rBlZXWZ1-MmsPhS/view?usp=sharing",
-    ],
-  },
-];
+  const email = "cybersecurity@dpsrkp.net";
+  const socialMedia = [
+    { platform: "Facebook", url: "https://www.facebook.com/CyberX" },
+    { platform: "Twitter", url: "https://twitter.com/CyberX" },
+    { platform: "LinkedIn", url: "https://www.linkedin.com/company/cyberx" },
+    { platform: "GitHub", url: "https://github.com/CyberX-club/" },
+    { platform: "Instagram", url: "https://www.instagram.com/CyberX.CLUB" },
+  ];
 
-const interviewData = {
-  title: "Principal's Interview",
-  subtitle: "Insights on Digital Ethics & Cyber Safety",
-  imageSrc: "https://i.imgur.com/xZ2fhmf.jpeg",
-  questions: [
-    "The concept of 'Dharma' in Hindu philosophy encourages fulfilling one’s duty with integrity and ethical conduct. In a world where online privacy and security are frequently compromised, what ethical values would you like to instil in our school community to navigate the digital space responsibly?",
-    "With social media having become such a big part of students’ lives, what advice would you give about staying secure and protecting privacy online?",
-    "In your opinion, what are the biggest challenges schools face when it comes to integrating technology effectively?",
-    "Have you ever had a personal experience with the digital world—whether it was dealing with a security issue, protecting your own digital information, or learning something new about online safety? We'd love to hear about any moments that highlighted the importance of cyber safety for you."
-  ],
-  answers: [
-    "Was Arjun a good or bad person? Was he right? Dharma is defined differently depending on one's viewpoint, the situation, and the moment. However, the core principles of Dharma educate us to act with integrity and truthfulness,They also inspire us to practise honesty and empathy. ",
-    "I would say to make the most of it. By this, I intend to use it as 'judiciously' as possible. Like beverages, it has the inherited ability to release dopamine. One must avoid becoming dependent on it. Never disclose passwords, keep your privacy settings strict, and refrain from disclosing personal details. Keep in mind that everything you do online creates a digital footprint, so choose wisely! Keep in mind that you made it. Instead of becoming its slave, be its ruler.",
-    "Inexperience. The overuse of technology is what is dangerous, not the technology itself.The most difficult task is striking a balance between the access to technology access and its thoughtful use. Although technology can improve learning, it's crucial to ensure that children are aware of the risks and utilise it sensibly. One of the most crucial issues to overcome is immaturity with regard to its use. The next step on the list is to accept your shortcomings.",
-    "Yes, I have experienced a similar situation, which made me see how easily private data may be compromised. I informed the police about the situation. "
-  ]
-};
+  const resources = [
+@@ -157,41 +159,43 @@
+    answers: [
+      "Absolutely! In Indian culture, we've always valued being cautious and prepared. We can apply this mindset by teaching students to think ahead by inculcating cyber safe practices like using strong passwords, avoiding suspicious links, and keeping personal information safe. It's all about making students aware of digital risks, so they can avoid problems before they happen.",
 
-export default function Main() {
+      "Cyber awareness is more important than ever. Our lives are so interconnected online, and any lack of awareness can lead to serious risks—like identity theft or hacking. If students understand basic cyber hygiene, like identifying phishing emails or being cautious with their passwords, they can avoid a lot of the common threats out there. It's about building a safer digital environment for everyone.",
+
+      "We can start by educating students about how cybersecurity is relevant to them. For example, explain how their social media privacy is at risk or how to keep their data safe. Hands-on activities like workshops, games, or competitions can make learning fun and interactive. The key is making it feel accessible and showing how anyone—whether they're tech-savvy or not—can play a role in staying secure online.",
+
+      "Students can make a big difference by promoting a safe and secure environment online and being role models for respectful behaviour. We can educate each other about the harms of cyberbullying and encourage students to speak up if they experience or witness it. When it comes to hacking, showing how tech skills can be used ethically—like in cyberX or hackathons—helps students understand the positive impact they can have with their abilities.",
+
+      "Students should adopt simple but effective habits—such as being extra cautious with their information, and being careful with what they share online. Schools can play a role by offering training and regular updates about digital security. The more we educate students about respecting privacy and being cautious online, the safer everyone will be."
+    ]
+  };
+
+
+
   return (
     <BrowserRouter>
-      <Navbar />
+      <Navbar config={navConfig} />
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/quiz" element={<Quiz />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/members" element={<CyberXApp />} />
-        <Route path="/alumni" element={<AlumniApp />} />
-        <Route path="/resources" element={<Resources resources={resources} />} />
-        <Route path="/magazine-entry" element={<MagzineEntry />} />
-        <Route path="/interview" element={<InterviewEntry data={interviewData} />} />
-        <Route path="/magazine" element={<Magazine />} />
+        <Route path={"/quiz"} element={<Quiz />} />
+        <Route path={"/members"} element={<CyberXApp />} />
+        <Route path={"/alumini"} element={<AlumniApp />} />
+        <Route path={"/about"} element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+
+        <Route
+          path={"/resources"}
+          element={<Resources resources={resources} />}
+        />
+        <Route path="/magazine-2024/:slug" element={<Magazine />} />
+        <Route path="/principals-message" element={<InterviewEntry {...interviewData}/>} />
+        <Route path="/vprincipal-m-s-message" element={<InterviewEntry {...vpcInterviewData}/>} />
+        <Route path="/vprincipal-a-s-message" element={<InterviewEntry {...vpCyberSecurityInterviewData}/>} />
+
       </Routes>
+
       <Footer email={email} socialMedia={socialMedia} />
     </BrowserRouter>
   );
-}
+};
+
+export default MyRoutes;
