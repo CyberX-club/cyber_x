83bae653975e0ab0016d14023dcdc42bfbfdee23
OR-6 <numair.khan18@gmail.com>
2025-10-11 20:03:41 +0530

    Members Update

--- src/components/Members.js
+++ src/components/Members.js
@@ -1,72 +1,59 @@
 import React from "react";
 import { Members, Alumni, CrypticText, Card3D } from "./PeopleBase";
 
-// Original membersData from old code
 const membersData = {
   12: [
-    { name: "Arahant Singh", role: "Core Member", image: "arahant.jpg" },
-    {
-      name: "Atharwa Navyam",
-      role: "Core Member",
-      image: "Atharwa Navyam 12F.jpg",
-    },
-    { name: "Arjun Atroley", role: "Core Member", image: "arjunatroley.jpg" },
-    {
-      name: "Swapnil Suraj",
-      role: "Core Member",
-      image: "swapnil.jpg",
-    },
+    { name: "Arahant Singh", role: "President", image: "arahant.jpg" },
+    {name: "Atharwa Navyam", role: "President", image: "Atharwa Navyam 12F.jpg"},
+    {name: "Aryaman Aggarwal", role: "Vice President", image: "aryaman aggarwal 11 I.jpeg"},
+    {name: "Naija Sukhija", role: "Vice President", image: "Naija Sukhija 11F.jpg"},
+    { name: "Arjun Atroley", role: "Director", image: "arjunatroley.jpg" },
     { name: "Goutam Behera", role: "Core Member", image: "Gouttam 11G.jpeg" },
     { name: "Numair Khan", role: "Core Member", image: "Numair.jpeg" },
-    {
-      name: "Aryaman Aggarwal",
-      role: "Core Member",
-      image: "aryaman aggarwal 11 I.jpeg",
-    },
-    { name: "Naija Sukhija", role: "Member", image: "Naija Sukhija 11F.jpg" },
+    { name: "Swapnil Suraj", role: "Core Member", image: "swapnil.jpg" },
     { name: "Sabeer Ranjan", role: "Member", image: "SABEER RANJAN.jpg" },
-    {
-      name: "Abhigyan Jhunjhunwala",
-      role: "Ad-Hoc",
-      image: "AbhigyanXIIC.jpg",
-    },
-    { name: "Shesh Shiromani", role: "Ad-Hoc", image: "Shesh12G.jpg" },
     { name: "Advaith Sai Panda", role: "Ad-Hoc", image: "advaith12H.png" },
+    {name: "Neil Saxena", role: "Ad-Hoc", image: "AbhigyanXIIC.jpg"},
+    { name: "Shesh Shiromani", role: "Ad-Hoc", image: "Shesh12G.jpg" }
   ],
+
   11: [
     { name: "Vidhan Garg", role: "Core Member", image: "Vidhan Garg 10C.png" },
     { name: "Vitabhay Garg", role: "Core Member", image: "Vitabhay Garg.jpg" },
-    { name: "Siddharth Srivastava", role: "Ad-Hoc", image: "" },
     { name: "Farzooque Hasan", role: "Ad-Hoc", image: "" },
     { name: "Aarya", role: "Ad-Hoc", image: "aarya11.png" },
     { name: "Elina Aggarwal", role: "Ad-Hoc", image: "Elina11.png" },
+    { name: "Naitik Singhal", role: "Ad-Hoc", image: "" },
+    { name: "Sanjit Krishna Agarwala", role: "Ad-Hoc", image: "" },
+    { name: "Siddharth Srivastava", role: "Ad-Hoc", image: "" },
   ],
+
   10: [
-    { name: "Devansh Kumar", role: "Member", image: "Devansh Kumar 9H.jpg" },
     { name: "Arhaan Sharma", role: "Member", image: "" },
+    { name: "Devansh Kumar", role: "Member", image: "Devansh Kumar 9H.jpg" },
+    { name: "Kiara Kapoor", role: "Member", image: "" },
     { name: "Aviral Goyal", role: "Ad-Hoc", image: "" },
     { name: "Esh Sinha", role: "Ad-Hoc", image: "" },
-    { name: "Kiara Kapoor", role: "Ad-Hoc", image: "" },
   ],
+
   9: [
     { name: "Aaratrika Singh", role: "Ad-Hoc", image: "" },
     { name: "Ayan Srivastava", role: "Ad-Hoc", image: "" },
   ],
 
   8: [
-    { name: "Amartya Samajhdar", role: "Ad-Hoc", image: " " },
-    { name: "Arshia Batheja", role: "Ad-Hoc", image: " " },
+    { name: "Amartya Samajhdar", role: "Ad-Hoc", image: " " }
   ],
 
   7: [
-    { name: "Anureet Kaur Sandhu", role: "Member", image: "Anureet Kaur Sandhu 6-K.jpg"},
-    { name: "Ranveer Kalra", role: "Member", image: "Ranveer Kalra 6d.png" },
+    { name: "Ranveer Kalra", role: "Member", image: "RanveerKalraUpd.png" },
     { name: "N Akshay Kumar", role: "Ad-Hoc", image: "Akshay7D.png" },
   ],
 
   6: [
-    { name: "Pranika Gupta", role: "Ad-Hoc", image: "" },
+    { name: "Aarav", role: "Ad-Hoc", image: "" },
     { name: "Keshav Karthikeye Valluri", role: "Ad-Hoc", image: "" },
+    { name: "Pranika Gupta", role: "Ad-Hoc", image: "" }
   ],
 };
 
